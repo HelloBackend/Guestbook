4a3fee8cc51376c8e26754d62a20071ae072b4db
HelloBackend <idbeo00@gmail.com>
2022-10-22 21:33:39 +0900

    방명록 서비스

--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -23,13 +23,11 @@ public class GuestbookRepositoryTests {
     private GuestbookRepository guestbookRepository;
 
     @Test
-    public void insertDummies(){
-
-        IntStream.rangeClosed(1,300).forEach(i -> {
-
+    public void insertDummies() {
+        IntStream.rangeClosed(1, 300).forEach(i -> {
             Guestbook guestbook = Guestbook.builder()
                     .title("Title...." + i)
-                    .content("Content..." +i)
+                    .content("Content..." + i)
                     .writer("user" + (i % 10))
                     .build();
             System.out.println(guestbookRepository.save(guestbook));
@@ -38,70 +36,43 @@ public class GuestbookRepositoryTests {
 
     @Test
     public void updateTest() {
-
         Optional<Guestbook> result = guestbookRepository.findById(300L); //존재하는 번호로 테스트
-
-        if(result.isPresent()){
-
+        if (result.isPresent()) {
             Guestbook guestbook = result.get();
-
             guestbook.changeTitle("Changed Title....");
             guestbook.changeContent("Changed Content...");
-
             guestbookRepository.save(guestbook);
         }
     }
 
     @Test
     public void testQuery1() {
-
         Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
-
         QGuestbook qGuestbook = QGuestbook.guestbook; //1
-
         String keyword = "1";
-
         BooleanBuilder builder = new BooleanBuilder();  //2
-
         BooleanExpression expression = qGuestbook.title.contains(keyword); //3
-
         builder.and(expression); //4
-
         Page<Guestbook> result = guestbookRepository.findAll(builder, pageable); //5
-
         result.stream().forEach(guestbook -> {
             System.out.println(guestbook);
         });
-
     }
 
     @Test
     public void testQuery2() {
-
         Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
-
         QGuestbook qGuestbook = QGuestbook.guestbook;
-
         String keyword = "1";
-
         BooleanBuilder builder = new BooleanBuilder();
-
-        BooleanExpression exTitle =  qGuestbook.title.contains(keyword);
-
-        BooleanExpression exContent =  qGuestbook.content.contains(keyword);
-
-        BooleanExpression exAll = exTitle.or(exContent); // 1----------------
-
-        builder.and(exAll); //2-----
-
-        builder.and(qGuestbook.gno.gt(0L)); // 3-----------
-
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+        BooleanExpression exContent = qGuestbook.content.contains(keyword);
+        BooleanExpression exAll = exTitle.or(exContent);
+        builder.and(exAll);
+        builder.and(qGuestbook.gno.gt(0L));
         Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
-
         result.stream().forEach(guestbook -> {
             System.out.println(guestbook);
         });
-
     }
-
 }
--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -11,73 +11,46 @@ import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
 public class GuestbookServiceTests {
-
     @Autowired
     private GuestbookService service;
 
     @Test
     public void testRegister() {
-
         GuestbookDTO guestbookDTO = GuestbookDTO.builder()
                 .title("Sample Title...")
                 .content("Sample Content...")
                 .writer("user0")
                 .build();
-
         System.out.println(service.register(guestbookDTO));
-
     }
 
-//    @Test
-//    public void testList(){
-//
-//        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
-//
-//        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
-//
-//        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
-//            System.out.println(guestbookDTO);
-//        }
-//    }
-
-
     @Test
-    public void testList(){
-
+    public void testList() {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
-
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
-
-        System.out.println("PREV: "+resultDTO.isPrev());
-        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
         System.out.println("TOTAL: " + resultDTO.getTotalPage());
-
         System.out.println("-------------------------------------");
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
-
         System.out.println("========================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 
-
     @Test
-    public void testSearch(){
-
+    public void testSearch() {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
                 .page(1)
                 .size(10)
-                .type("tc")   //검색 조건 t, c, w, tc, tcw ..
-                .keyword("한글")  // 검색 키워드
+                .type("tc")
+                .keyword("한글")
                 .build();
-
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
-
-        System.out.println("PREV: "+resultDTO.isPrev());
-        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
         System.out.println("TOTAL: " + resultDTO.getTotalPage());
-
         System.out.println("-------------------------------------");
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
@@ -86,8 +59,4 @@ public class GuestbookServiceTests {
         System.out.println("========================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
-
-
-
-
 }
